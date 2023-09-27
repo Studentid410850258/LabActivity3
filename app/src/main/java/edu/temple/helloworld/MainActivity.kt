f4e332c02058650b60780453e97157f4a2771da9
Studentid410850258 <134401041+Studentid410850258@users.noreply.github.com>
2023-09-28 04:37:03 +0800

    FEATURE-1

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
     lateinit var nameET: EditText
+    lateinit var clickMe: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +19,20 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
-        nameET = findViewById<EditText>(R.id.nameEditText)
+        nameET = findViewById(R.id.nameEditText)
+        clickMe = findViewById(R.id.clickMeButton)
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${nameET.text}"
+            val text = nameET.text.toString()
+
+            if(text.isEmpty()) {
+                displayTextView.text = ""
+                nameET.setError("No name detected!")
+            } else {
+                displayTextView.text = "Hello, ${nameET.text}"
+            }
+
+
         }
 
 
